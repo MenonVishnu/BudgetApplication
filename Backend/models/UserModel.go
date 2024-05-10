dd6d67cd0ecc19be37c8215e2d4879ae385dbba3
Vishnu Menon <menonvishnu26@gmail.com>
2024-05-10 16:37:12 +0530

    Model

---
+++ Backend/models/UserModel.go
@@ -0,0 +1,11 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name     string             `json:"name,omitempty"`
+	Email    string             `json:"email,omitempty"`
+	Password string             `json:"password,omitempty"`
+	Role     string             `json:"role,omitempty"`
+}
