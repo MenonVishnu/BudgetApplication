413ac13a1d85ac0d39fcba33aa50c043509def94
Vishnu Menon <menonvishnu26@gmail.com>
2024-05-28 22:13:42 +0530

    Added comments for reference

--- Backend/controllers/UserController.go
+++ Backend/controllers/UserController.go
@@ -14,8 +14,10 @@ func AddUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Allow-Control-Allow-Methods", "POST")
 
+	//creating a reference model variable
 	var user models.User
 
+	//decoding the JSON and assigning those values into the reference model variable
 	_ = json.NewDecoder(r.Body).Decode(&user)
 	database.AddUser(user)
 	fmt.Println(user.ID)
--- Backend/models/BudgetModel.go
+++ Backend/models/BudgetModel.go
@@ -2,7 +2,6 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-
 type Budget struct {
 	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty""`
 	Amount int                `json:"amount,omitempty"`
