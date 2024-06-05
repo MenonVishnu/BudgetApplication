ff641a7522434053d7ddcd32b67ff15e751df266
Vishnu Menon <menonvishnu26@gmail.com>
2024-06-05 15:07:16 +0530

    Update Budget Helper Function

--- Backend/controllers/BudgetController.go
+++ Backend/controllers/BudgetController.go
@@ -5,8 +5,10 @@ import (
 	"net/http"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/gorilla/mux"
 	"github.com/vishnumenon/budgetapplication/database"
 	"github.com/vishnumenon/budgetapplication/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func AddBudget(w http.ResponseWriter, r *http.Request) {
@@ -31,4 +33,29 @@ func AddBudget(w http.ResponseWriter, r *http.Request) {
 	budget.ID = database.AddBudget(budget)
 	message := "Budget created Successfully with ObjectId: " + budget.ID.Hex()
 	models.SuccessResponse(w, 201, message, budget)
+}
+
+func UpdateBudget(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Allow-Control-Allow-Methods", "PUT")
+
+	params := mux.Vars(r)
+	var budget models.Budget
+
+	_ = json.NewDecoder(r.Body).Decode(&budget)
+
+	//Validation of updated user
+	validate := validator.New()
+	err := validate.Struct(budget)
+	if err != nil {
+		errors := err.(validator.ValidationErrors)
+		models.ErrorResponse(w, 403, "Vallidation Error!!", errors.Error())
+		return
+	}
+
+	database.UpdateBudget(budget, params["id"])
+	budget.ID, _ = primitive.ObjectIDFromHex(params["id"])
+	message := "User updated Successfully with ObjectId: " + budget.ID.Hex()
+	models.SuccessResponse(w, 201, message, budget)
+
 }
--- Backend/database/BudgetDatabaseHelper.go
+++ Backend/database/BudgetDatabaseHelper.go
@@ -6,9 +6,11 @@ import (
 	"log"
 
 	"github.com/vishnumenon/budgetapplication/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+// add budget to database
 func AddBudget(budget models.Budget) primitive.ObjectID {
 	inserted, err := BudgetCollection.InsertOne(context.Background(), budget)
 
@@ -18,6 +20,32 @@ func AddBudget(budget models.Budget) primitive.ObjectID {
 	//changing data type from interface to primitive.objectId
 	budgetId := inserted.InsertedID.(primitive.ObjectID)
 
-	fmt.Println("User has been inserted Successfully with ID: ", budgetId)
+	fmt.Println("Budget has been inserted Successfully with ID: ", budgetId)
 	return budgetId
+}
+
+// update budget to database
+func UpdateBudget(budget models.Budget, budgetId string) {
+	id, err := primitive.ObjectIDFromHex(budgetId)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{
+		"amount": budget.Amount,
+		"tags":   budget.Tags,
+		"Date":   budget.Date,
+		"User":   budget.User,
+	}}
+
+	updatedBudget, err := BudgetCollection.UpdateOne(context.Background(), filter, update)
+
+	if err != nil {
+		log.Fatal(updatedBudget)
+	}
+
+	fmt.Println("Budget Updated with object ID: ", budgetId, " Documents Affected: ", updatedBudget.ModifiedCount)
+
 }
